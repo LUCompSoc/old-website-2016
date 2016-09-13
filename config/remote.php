fa364c61907ec3d39e47d7f92239232a01ecc3c1
escproxy <a.jung@lancaster.ac.uk>
2016-09-13 01:47:50 +0100

    Added deployment tool

--- config/remote.php
+++ config/remote.php
@@ -6,7 +6,6 @@ return [
             'host'      => env('SSH_HOST', 'http://localhost'),
             'username'  => env('SSH_USERNAME', 'root'),
             'password'  => env('SSH_PASSWORD', 'root'),
-            'password'  => env('SSH_PASSWORD', 'root'),
             'key'       => '',
             'keyphrase' => '',
             'root'      => env('SSH_ROOT', '/var/www/html'),
