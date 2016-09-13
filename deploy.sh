8b6a8549303ab3967acaef03abf6ef20fe1a1227
escproxy <a.jung@lancaster.ac.uk>
2016-09-13 16:29:36 +0100

    Added the server deployment script

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# This file should be run on the server to pull the latest version of the repo
+
+# Make sure only root can run our script
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+echo "Pulling latest version from the GitHub repo..."
+git checkout master
+git fetch --all
+git reset --hard origin/master
+chgrp -R www-data .
